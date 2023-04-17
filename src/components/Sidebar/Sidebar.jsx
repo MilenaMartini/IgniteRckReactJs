8a017ae79dffabf6ff92b9fdee5fb8fac5847879
MilenaMartini <martinimilena8@gmail.com>
2023-04-17 17:20:13 -0300

    atualização sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,11 +5,13 @@ export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img className={styles.cover}
-      src="https://images.unsplash.com/photo-1680458842171-6dbeced420f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
+      src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjbm9sb2dpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
 
     <div className={styles.profile}>
-    <strong > Nome </strong>
-    <span> Profissão</span>
+    <img className={styles.avatar} src="https://github.com/MilenaMartini.png"/>
+
+    <strong > Milena Martini </strong>
+    <span> Desenvolvedora Front-end </span>
     </div>
 
     <footer>
