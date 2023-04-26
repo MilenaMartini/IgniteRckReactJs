35e17f64ee908e1bc917e72d96c03a2a2ec87983
MilenaMartini <martinimilena8@gmail.com>
2023-04-26 12:23:48 -0300

    Atualização Avatar

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -5,7 +5,7 @@ import { Avatar } from '../Avatar/Avatar'
 export function Comment () {
   return (
     <div className={styles.comment}>
-      <Avatar src="https://github.com/MilenaMartini.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/MilenaMartini.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
